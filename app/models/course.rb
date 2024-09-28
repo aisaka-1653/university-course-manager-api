01753035c4025b7304fb6c1c49d006912e90eb64
aisaka-1653 <reobata1127@gmail.com>
2024-09-28 16:17:11 +0900

    feat: User､Course､Teacherのアソシエーションを設定

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class Course < ApplicationRecord
+  has_many :teacher_courses, dependent: :destroy
+  has_many :teachers, through: :teacher_courses
 end
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,4 +2,6 @@
 
 class Teacher < ApplicationRecord
   belongs_to :user
+  has_many :teacher_courses, dependent: :destroy
+  has_many :courses, through: :teacher_courses
 end
