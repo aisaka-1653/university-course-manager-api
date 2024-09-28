5491a687e196afef2d2cd6d4ae8d83916c8a1493
aisaka-1653 <reobata1127@gmail.com>
2024-09-28 17:16:22 +0900

    feat: seedで初期データを投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,39 @@
 # frozen_string_literal: true
 
-3.times do |_n|
-  user = User.new(
-    email: Faker::Internet.unique.email,
-    password: 123_456,
-    first_name: Faker::Name.last_name,
-    last_name: Faker::Name.first_name,
-    role: '学生'
-  )
-  user.skip_confirmation!
-  user.save!
+require_relative 'seeds/courses'
+
+student = User.new(
+  email: Faker::Internet.unique.email,
+  password: 123_456,
+  first_name: Faker::Name.last_name,
+  last_name: Faker::Name.first_name,
+  role: 'student'
+)
+student.skip_confirmation!
+student.save!
+
+10.times do |n|
+  ActiveRecord::Base.transaction do
+    user = User.new(
+      email: Faker::Internet.unique.email,
+      password: 123_456,
+      first_name: Faker::Name.last_name,
+      last_name: Faker::Name.first_name,
+      role: 'teacher'
+    )
+    user.skip_confirmation!
+    user.save!
+
+    teacher = user.build_teacher(
+      position: 'professor'
+    )
+    teacher.save!
+
+    course = COURSES[n]
+    teacher.courses.create!(
+      name: course[:name],
+      description: course[:description],
+      semester: course[:semester]
+    )
+  end
 end
